d2aa5eb67497250fd061a593fb0d6d61d5403fab
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2025-11-17 06:56:35 +0530

    Designed schema

---
+++ Backend/src/prisma/migrations/20251117012612_added_tables/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Todos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `todo` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Todos` ADD CONSTRAINT `Todos_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
